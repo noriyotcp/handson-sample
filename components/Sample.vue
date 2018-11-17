4346386cd371d59105027c0c00f67ad0e85bfb56
noriyotcp <noriyo.akita@gmail.com>
2018-11-17 16:09:50 +0900

    Add Sample.vue

---
+++ components/Sample.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="sample">
+    Hello, I'm {{ name }}!
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'Noriyo Akita'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
